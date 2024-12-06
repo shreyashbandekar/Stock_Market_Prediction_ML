e1e0b195167dac5a07d481d6296ee4a45bb41c70
shreyashbandekar <shreyash.bandekar2015@gmail.com>
2024-12-06 18:00:38 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+from keras.models import load_model # type: ignore
 import streamlit as st
 import matplotlib.pyplot as plt
 
